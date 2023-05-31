82164e973fb61046237bb3b0b1646d1ce8693303
LucaPaolella <paolellaluca99@gmail.com>
2023-05-31 12:28:11 +0200

    creazione app CardList e MainBot

---
+++ src/components/AppMainBot.vue
@@ -0,0 +1,18 @@
+<script>
+import AppCardList from "./components/AppCardList.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+    name: "App",
+    components: {
+        AppCardList,
+        AppFooter,
+    },
+};
+</script>
+
+<template>
+    <AppCardList />
+
+    <AppFooter />
+</template>
