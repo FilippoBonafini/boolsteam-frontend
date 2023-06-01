3b79e071dd7cf50a53e4b0eaf76939fbd9a752be
FilippoBonafini <filippo.bonafini.99@icloud.com>
2023-06-01 09:52:58 +0200

    Aggiunti elementi e modificata grarfica

--- src/components/AppCardList.vue
+++ src/components/AppCardList.vue
@@ -22,32 +22,31 @@ export default {
         >
           <!--il contaier img poi va tolto-->
           <div class="info-img d-flex">
-
             <div class="container-img">
               <img :src="store.img_prefix + game.image" alt="" />
-
             </div>
 
             <div class="info">
-
               <h3>{{ game.title }}</h3>
               <div class="d-flex gap-2">
                 <div class="tag" v-for="tag in game.genres">
                   {{ tag.name }}
                 </div>
-
               </div>
 
               <div>
-
                 <small>{{ game.release_date }}</small
                 ><!--aggiungere l'icona delle piattaforme-->
-
               </div>
             </div>
           </div>
-
-          <div class="discount px-2">{{ game.sconto }}%</div>
+          <div class="d-flex align-items-center gap-1">
+            <div class="discount">-{{ game.sconto }}%</div>
+            <div class="discount-price text-center">
+              <div>{{ game.price }}$</div>
+              <div class="final-price">122</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -61,20 +60,11 @@ export default {
 main {
   background-color: $main-bg;
   color: $card-text;
-}
-
-.container-main-bot>h3 {
-  color: white;
-}
-
-.container-card-list {
-  background-color: $card-bg;
-  padding-top: 5px;
-  box-shadow: 0px -2px 4px rgba(0, 0, 0, 0.3);
-}
-
-.container-card-list:last-child {
   padding-bottom: 30px;
+}
+
+.container-main-bot > h3 {
+  color: white;
 }
 
 .container-img {
@@ -87,6 +77,12 @@ main {
   }
 }
 
+.card-film {
+  box-shadow: 0px -2px 4px rgba(0, 0, 0, 0.3);
+  background-color: $card-bg;
+  margin-bottom: 15px;
+}
+
 .tag {
   background-color: $card-tag-bg;
   text-align: center;
@@ -96,7 +92,29 @@ main {
   color: $card-text-discount;
   background-color: $card-bg-bg-discount;
   font-size: 25px;
-  margin-bottom: 5px;
-  margin-right: 5px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 10px;
+}
+.discount-price {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 15px;
+  height: 40px;
+  font-size: 14px;
+  line-height: 20px;
+  background-color: $card-bg-discount-price;
+}
+
+.final-price {
+  color: $card-text-discount;
+  font-weight: bolder;
+}
+.final-price::after {
+  content: "$";
 }
 </style>
